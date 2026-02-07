cdeb0c20e6ac81b8eaa3c85bb58568d4e6b0c6e0
you-ssef-dan <youssefdanoun02@gmail.com>
2026-02-07 10:36:30 +0100

    file processing done

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -3,8 +3,9 @@ from enum import Enum
 class ResponseSignal(Enum):
 
     FILE_VALIDATED_SUCCESSFULLY = "file_validate_successfully"
-
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCCESSFULLY = "file_upload_successfully"
     FILE_UPLOAD_FAILED = "file_upload_failed"
+    PROCESSING_SUCCESSFULLY = "processing_success"
+    PROCESSING_FAILED = "processing_failed"
